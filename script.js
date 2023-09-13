25252cb4a9ea230f40689648ba53794b7b1eb4f2
Regina Gering <Regina.Gering@hotmail.de>
2023-09-13 16:58:34 +0200

    added pol

--- script.js
+++ script.js
@@ -39,24 +39,43 @@ let questions = [
         "answer_4": "ol",
         "right_answer": 3
     },
-
 ];
 
 
+let scoreCount = 0;
 let currentQuestion = 0;
+
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
+
+
 
 function init() {
     let amountQuestion = document.getElementById('amount-quest');
     amountQuestion.innerHTML = questions.length;
 
     showQuestion()
+    // showPol();
 }
 
 
 function showQuestion() {
-    if(currentQuestion >= questions.length) {
-        // do something
+    if(gameIsOver()) {
+        showPolResult();
+        showResult();
     } else {
+        showPol();
+        askQuestion();
+    }
+}
+
+
+function gameIsOver() {
+    return currentQuestion >= questions.length
+}
+
+
+function askQuestion() {
     let question = questions[currentQuestion];
     let questionText = document.getElementById('question-text');
     questionText.innerHTML = question["question"];
@@ -70,33 +89,48 @@ function showQuestion() {
     answer_3.innerHTML = question["answer_3"];
     let answer_4 = document.getElementById('answer4');
     answer_4.innerHTML = question["answer_4"];
-    }
+}
+
+function showPol() {
+    let percent = (currentQuestion+1) / (questions.length+1);
+    percent = Math.round(percent * 100);
+    console.log(`Fortschritt: ${percent}`);
+    document.getElementById('progress-bar').innerHTML = `${percent} %`;
+    document.getElementById('progress-bar').style = `width: ${percent}%;`;
+}
+
+function showPolResult() {
+    document.getElementById('progress-bar').innerHTML = "100 %";
+    document.getElementById('progress-bar').style = "width: 100%;";
 }
 
 
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
-    let rightAnswer = document.getElementById(`con-answer${selectedQuestionNumber}`);
-
     let idOfRightAnswer = `answer${question["right_answer"]}`
 
     if(selectedQuestionNumber == question["right_answer"]) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        scoreCount++;
+        AUDIO_SUCCESS.play();
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
     document.getElementById('next-button').disabled = false;
 }
 
 function nextQuestion() {
     currentQuestion++;
+    // colorPol();
+
     showQuestion();
 
     document.getElementById('next-button').disabled = false;
 
-    resetButton()
+    resetButton();
 }
 
 
@@ -105,4 +139,52 @@ function resetButton() {
         document.getElementById(`answer${i}`).parentNode.classList.remove('bg-danger');
         document.getElementById(`answer${i}`).parentNode.classList.remove('bg-success');
     }
+}
+
+
+function showResult() {
+    let cardResult = document.getElementById('card-body-result');
+    cardResult.style = "";    
+    let cardBody = document.getElementById('card-body');
+    cardBody.style = 'display: none';
+
+    showScore();
+}
+
+
+function showScore() {
+    let scoreNumber = document.getElementById('score-number');
+    scoreNumber.innerHTML = `${scoreCount}/${questions.length}`;
+
+}
+
+
+// function showPol() {
+//     let questionPol = document.getElementById('question-pol');
+    
+//     if(questionPol.innerHTML.length != questions.length+1){
+//     for (let i = 0; i < questions.length+1; i++) {
+//         questionPol.innerHTML += `<span id="pol-color-done-${i}" class="pol-color"></span>`;
+//     }}
+//     colorPol();
+// }
+
+// function colorPol() {
+//     let polColorDone = document.getElementById(`pol-color-done-${currentQuestion}`);
+//     polColorDone.style.backgroundColor = "rgba(0,0,0,0.25)";
+// }
+
+function restartGame() {
+    document.getElementById('card-body-result').style = 'display: none';    
+    document.getElementById('card-body').style = "";
+
+    scoreCount = 0;
+    currentQuestion = 0;
+
+    // for (let i = 0; i < questions.length+1; i++) {
+    //     let polColorDone = document.getElementById(`pol-color-done-${i}`);
+    //     polColorDone.style.backgroundColor = "";
+    // }
+
+    init();
 }
